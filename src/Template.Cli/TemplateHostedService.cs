c379e2402667e9857f95c5c0a428a3f1b891def9
Anton Bauer <bauer_av@proton.me>
2024-08-25 15:28:23 +0200

    add host builder

--- src/Template.Cli/TemplateHostedService.cs
+++ src/Template.Cli/TemplateHostedService.cs
@@ -2,5 +2,13 @@ using Microsoft.Extensions.Hosting;
 
 internal sealed class TemplateHostedService : IHostedService
 {
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
 }
