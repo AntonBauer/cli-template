548218b1bf78e152eb708c8cf839091c28f16824
Anton Bauer <bauer_av+git@proton.me>
2024-08-25 20:40:01 +0200

    add hosted service, run host

--- src/Template.Cli/Program.cs
+++ src/Template.Cli/Program.cs
@@ -1,3 +1,8 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<TemplateHostedService>();
+
+var host = builder.Build();
+await host.RunAsync();
